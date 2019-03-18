1c486e2f00fca28563875ceb821a02d6505f61bb
Leah Boyd <leahboyd08@gmail.com>
2019-03-18 14:03:23 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -51,13 +51,12 @@ end
 
 def runner
   welcome
-  deal_card
+  initial_round
+  hit?(current_card_total)
   display_card_total(card_total)
+  deal_card
   prompt_user
   get_user_input
   end_game(card_total)
-  initial_round
-  hit?(current_card_total)
-  invalid_command
 end
     
